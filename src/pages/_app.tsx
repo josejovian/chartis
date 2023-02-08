77057e3b95651139a4f6b579d17c4d5b9737fdc6
Jose Jovian <josejovian55@gmail.com>
2023-02-08 19:43:12 +0700

    feat: LayoutNavbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { LayoutNavbar } from "@/components";
+import { Lato } from "@next/font/google";
+import "semantic-ui-css/semantic.min.css";
+
+const lato = Lato({ subsets: ["latin"], weight: ["400", "700", "900"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<style jsx global>{`
+				html {
+					font-family: ${lato.style.fontFamily};
+				}
+			`}</style>
+			<div className="flex flex-row">
+				<LayoutNavbar />
+				<Component {...pageProps} />
+			</div>
+		</>
+	);
 }
