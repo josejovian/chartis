c239f8e74c6368397817d0397898744fbe76090b
Jose Jovian <josejovian55@gmail.com>
2023-03-05 10:48:43 +0700

    chore: fix date difference output

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -29,41 +29,47 @@ export function strDateTime(date: Date) {
 }
 
 export function getTimeDifference(timeBefore: number, timeAfter?: number) {
-  if (!timeBefore) return `unktimeAftern`;
+  if (!timeBefore) return `unknown time`;
 
   if (!timeAfter) timeAfter = new Date().getTime();
 
-  const difference = Math.abs(Math.ceil((timeAfter - timeBefore) / 1000));
+  const string = (() => {
+    const difference = Math.abs(Math.ceil((timeAfter - timeBefore) / 1000));
 
-  if (difference < 60) {
-    return `less than a minute`;
-  } else if (difference < 60 * 60) {
-    const timeBefore = Math.floor(difference / 60);
-    return `${Math.floor(difference / 60)} minute${timeBefore > 1 ? "s" : ""}`;
-  } else if (difference < 60 * 60 * 24) {
-    const timeBefore = Math.floor(difference / (60 * 60));
-    return `${Math.floor(difference / (60 * 60))} hour${
-      timeBefore > 1 ? "s" : ""
-    }`;
-  } else if (difference < 60 * 60 * 24 * 7) {
-    const timeBefore = Math.floor(difference / (60 * 60 * 24));
-    return `${Math.floor(difference / (60 * 60 * 24))} day${
-      timeBefore > 1 ? "s" : ""
-    }`;
-  } else if (difference < 60 * 60 * 24 * 7 * 30) {
-    const timeBefore = Math.floor(difference / (60 * 60 * 24 * 7));
-    return `${Math.floor(difference / (60 * 60 * 24 * 7))} week${
-      timeBefore > 1 ? "s" : ""
-    }`;
-  } else if (difference < 60 * 60 * 24 * 7 * 30 * 12) {
-    const timeBefore = Math.floor(difference / (60 * 60 * 24 * 7 * 30));
-    return `${Math.floor(difference / (60 * 60 * 24 * 7 * 30))} month${
-      timeBefore > 1 ? "s" : ""
-    }`;
-  } else {
-    const timeBefore = Math.floor(difference / (60 * 60 * 24 * 7 * 30 * 12));
-    return `${Math.floor(difference / (60 * 60 * 24 * 7 * 30 * 12))} year${
-      timeBefore > 1 ? "s" : ""
-    }`;
-  }
+    if (difference < 60) {
+      return `less than a minute`;
+    } else if (difference < 60 * 60) {
+      const timeBefore = Math.floor(difference / 60);
+      return `${Math.floor(difference / 60)} minute${
+        timeBefore > 1 ? "s" : ""
+      }`;
+    } else if (difference < 60 * 60 * 24) {
+      const timeBefore = Math.floor(difference / (60 * 60));
+      return `${Math.floor(difference / (60 * 60))} hour${
+        timeBefore > 1 ? "s" : ""
+      }`;
+    } else if (difference < 60 * 60 * 24 * 7) {
+      const timeBefore = Math.floor(difference / (60 * 60 * 24));
+      return `${Math.floor(difference / (60 * 60 * 24))} day${
+        timeBefore > 1 ? "s" : ""
+      }`;
+    } else if (difference < 60 * 60 * 24 * 7 * 30) {
+      const timeBefore = Math.floor(difference / (60 * 60 * 24 * 7));
+      return `${Math.floor(difference / (60 * 60 * 24 * 7))} week${
+        timeBefore > 1 ? "s" : ""
+      }`;
+    } else if (difference < 60 * 60 * 24 * 7 * 30 * 12) {
+      const timeBefore = Math.floor(difference / (60 * 60 * 24 * 7 * 30));
+      return `${Math.floor(difference / (60 * 60 * 24 * 7 * 30))} month${
+        timeBefore > 1 ? "s" : ""
+      }`;
+    } else {
+      const timeBefore = Math.floor(difference / (60 * 60 * 24 * 7 * 30 * 12));
+      return `${Math.floor(difference / (60 * 60 * 24 * 7 * 30 * 12))} year${
+        timeBefore > 1 ? "s" : ""
+      }`;
+    }
+  })();
+
+  return timeAfter < timeBefore ? `in ${string}` : `${string} ago`;
 }
