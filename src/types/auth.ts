4785a626e6215f441cfaff2a88d259228ae63c21
Jose Jovian <josejovian55@gmail.com>
2023-04-28 23:46:07 +0700

    chore: store email/join date on register

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,11 +1,21 @@
 import { type User } from "firebase/auth";
 import { USER_TYPES } from "@/consts";
 
-export interface UserType {
+export type UserRoleType = "admin" | "user";
+
+export interface UserDatabaseType {
   name: string;
+  email: string;
+  joinDate: number;
   subscribedEvents?: Record<string, number>;
   unseenEvents?: Record<string, boolean>;
   notificationCount?: number;
+  role?: UserRoleType;
+  ban?: boolean;
+}
+
+export interface UserType extends UserDatabaseType {
+  id?: string;
 }
 
 export type UserObjectType = User | null;
