b3ac03d8d23d48982eb38670ab54fadd202c163e
Jose Jovian <josejovian55@gmail.com>
2023-04-30 18:07:38 +0700

    fix: deploy error

--- src/consts/auth.ts
+++ src/consts/auth.ts
@@ -1,13 +1,1 @@
 export const USER_TYPES = ["guest", "user", "admin"] as const;
-
-export const USER_: EventType = {
-  id: "0",
-  authorId: "admin",
-  authorName: "admin",
-  name: "December Monthly Report Deadline",
-  description: "Reminder pengumpulan laporan enrichment untuk bulan Desember.",
-  postDate: 0,
-  startDate: 0,
-  endDate: 10,
-  tags: {},
-};
